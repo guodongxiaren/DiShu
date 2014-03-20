0cf78cb27f289a80b9b4a196229be4ffa3cd5e9d
guodongxiaren <879231132@qq.com>
2014-03-20 21:50:55 +0800

    test2

--- src/com/jelly/dishu/AboutActivity.java
+++ src/com/jelly/dishu/AboutActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
